4bad08b1b35d3dcd952ec79cad72c9fb2b4fd50e
MFbuider <62496746+MFbuider@users.noreply.github.com>
2020-11-14 17:06:41 +0800

    C#
    類型

---
+++ 2D_game/Assets/script/c.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class c : MonoBehaviour
+{
+    [Header("cc"), Tooltip("用來設定cc的。")]
+    public int cc = 64301;
+public float g = 1000f;
+public  string a = "bc";
+    public bool haveatree = true;
+    public Color red = Color.red;
+    public Color clorw = new Color(0, 2, 0);
+   public Vector2 v2 = Vector2.one;
+   public Vector3 v3 = Vector3.one;
+   public Vector4 v4 = Vector4.one;
+   public GameObject obj;
+   public Terrain tr;
+   public SpriteRenderer sr ;
+}
