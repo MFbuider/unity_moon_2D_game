8deb39d1f7ab5e29e7c787d7841183c88192c51a
MFbuider <62496746+MFbuider@users.noreply.github.com>
2020-11-21 21:23:43 +0800

    PC(exe)

---
+++ 2D_game/Assets/script/MenuManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuManager : MonoBehaviour
+{
+    
+
+    // Start is called before the first frame update
+    public void StartGame ()
+    {
+        SceneManager.LoadScene("關卡1");
+    }
+
+    // Update is called once per frame
+    public  void QuitGame()
+    {
+        Application.Quit();
+    }
+}
--- 2D_game/Assets/script/plear.cs
+++ 2D_game/Assets/script/plear.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class plear : MonoBehaviour
 {
     [Header("速度"), Tooltip("用來設定移動的速度。")]
-    public float Movingspeed = 10.5f;[Range(0, 1000)]
+    public float speed = 10.5f;[Range(0, 1000)]
     [Header("跳躍"), Tooltip("用來設定跳躍的高度。")]
     public float jump = 100f;[Range(0, 3000)]
     public bool floor = false;
@@ -17,19 +17,29 @@ public class plear : MonoBehaviour
     public float Movingpeed = 800f;[Range(0, 5000)]
     public AudioClip bulletAU;
     public int lives = 3;[Range(0, 5)]
-    bool AudioSource = false;
-    bool RD= false;
-    bool nimation = false;
+    private AudioSource aud;
+    private Rigidbody2D rig;
+    private Animator ani;
+
+    private void Awake()
+    {
+        rig = GetComponent<Rigidbody2D>(); 
+    }
+    private void Update()
+    {
+        Move();   
+    }
     private void Move()
     {
-
+        float L = Input.GetAxis("Horizontal");
+        rig.velocity = new Vector2(L * speed, rig.velocity.y);
     }
     /// <summary>
     /// 跳躍
     /// </summary>
     public void jump2()
     {
-
+   
     }
     /// <summary>
     /// 開槍
@@ -37,5 +47,9 @@ public class plear : MonoBehaviour
     public void fire()
     {
 
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
     }
 }
